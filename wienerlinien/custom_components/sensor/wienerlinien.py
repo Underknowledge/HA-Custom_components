cdf4376952ac1d1a0fc318205e970f409e7d2f86
Joakim Sørensen <ludeeus@users.noreply.github.com>
2018-05-20 19:21:32 +0200

    wienerlinien 1.0.1 - Added next departure as attribute

--- wienerlinien/custom_components/sensor/wienerlinien.py
+++ wienerlinien/custom_components/sensor/wienerlinien.py
@@ -16,6 +16,7 @@ CONF_STOPID = 'stopid'
 CONF_APIKEY = 'apikey'
 
 ATTR_STOPID = 'stopid'
+ATTR_NEXT_DEPARTURE = 'next_departure'
 ATTR_COMPONENT = 'component'
 ATTR_COMPONENT_VERSION = 'component_version'
 
@@ -23,7 +24,7 @@ SCAN_INTERVAL = timedelta(seconds=30)
 
 ICON = 'mdi:bus'
 COMPONENT_NAME = 'wienerlinien'
-COMPONENT_VERSION = '1.0.0'
+COMPONENT_VERSION = '1.0.1'
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_STOPID): cv.string,
@@ -42,6 +43,7 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 class WienerlinienSensor(Entity):
     def __init__(self, stopid, apikey):
         self._state = 'N/A'
+        self._nextdeparture = 'N/A'
         self._stopid = stopid
         self._apikey = apikey
         self._component = COMPONENT_NAME
@@ -58,14 +60,17 @@ class WienerlinienSensor(Entity):
                 departure['monitors'][0]['lines'][0]['departures']['departure'][0]['departureTime']['countdown']
             except:
                 countdown = 'N/A'
+                nextDeparture = 'N/A'
             else:
                 countdown = departure['monitors'][0]['lines'][0]['departures']['departure'][0]['departureTime']['countdown']
+                nextDeparture = departure['monitors'][0]['lines'][0]['departures']['departure'][1]['departureTime']['countdown']
             self._state = countdown
+            self._nextdeparture = nextDeparture
 
 
     @property
     def name(self):
-        return 'Next departure'
+        return 'Wienerlinien - Departure'
 
     @property
     def state(self):
@@ -76,8 +81,13 @@ class WienerlinienSensor(Entity):
         return ICON
 
     @property
+    def unit_of_measurement(self):
+        return 'min(s)'
+
+    @property
     def device_state_attributes(self):
         return {
+            ATTR_NEXT_DEPARTURE: self._nextdeparture,
             ATTR_STOPID: self._stopid,
             ATTR_COMPONENT: self._component,
             ATTR_COMPONENT_VERSION: self._componentversion
